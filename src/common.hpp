454d74be37bb8b64564e12d70165e88eeb22ca13
JustMichael95 <adrfranklin95@gmail.com>
2020-03-17 09:45:41 +0000

    Removed old macro functions

--- src/common.hpp
+++ src/common.hpp
@@ -1,26 +1,5 @@
 extern void** ppPluginData;
 extern void* pAMXFunctions;
 
-#define TRY(exp) do { \
-    napi_status status; \
-    status = exp; \
-    if (status != napi_ok) { \
-        napi_throw_error(env, NULL, "N-API call failed"); \
-    } \
-} while(0)
-
-#define REGISTER_METHOD(env, object, name, func) do { \
-    napi_value fn; \
-    TRY(napi_create_function(env, NULL, 0, func, NULL, &fn)); \
-    TRY(napi_set_named_property(env, object, name, fn)); \
-} while(0)
-
-#define REGISTER_MEMBER(type, env, object, name, value) do { \
-    napi_value val; \
-    TRY(napi_create_##type (env, value, &val)); \
-    TRY(napi_set_named_property(env, object, name, val)); \
-} while (0)
-
-
 #include "logger.hpp"
 #include "utils.hpp"
