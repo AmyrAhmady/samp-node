44b2a7599033fc7cabcd510d7ab495ab36dec9a0
Amyr Ahmady <amirabgta@gmail.com>
2020-03-27 10:27:38 +0430

    remove unnecessary new lines, following standard code style guid

--- src/main.cpp
+++ src/main.cpp
@@ -26,9 +26,7 @@ PLUGIN_EXPORT bool PLUGIN_CALL OnPublicCall(AMX* amx, const char* name, cell* pa
 PLUGIN_EXPORT void PLUGIN_CALL ProcessTick()
 {
 	sampgdk::ProcessTick();
-
 	sampnode::node_tick();
-
 	return;
 }
 
@@ -40,7 +38,6 @@ PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports()
 PLUGIN_EXPORT bool PLUGIN_CALL Load(void** ppData)
 {
 	logprintf = (logprintf_t)(ppData[PLUGIN_DATA_LOGPRINTF]);
-
 	pAMXFunctions = ppData[PLUGIN_DATA_AMX_EXPORTS];
 
 	if(!sampnode::Config::Get()->ParseFile())
@@ -52,33 +49,26 @@ PLUGIN_EXPORT bool PLUGIN_CALL Load(void** ppData)
 
 	Log::Init();
 	sampgdk::Load(ppData);
-
 	sampnode::callback::init();
-
 	sampnode::node_init();
-
 	return true;
 }
 
 PLUGIN_EXPORT int PLUGIN_CALL AmxLoad(AMX* amx)
 {
 	sampnode::amx::load(amx);
-
 	return 1;
 }
 
 PLUGIN_EXPORT void PLUGIN_CALL Unload()
 {
 	sampgdk::Unload();
-
 	sampnode::node_stop();
-
 	return;
 }
 
 PLUGIN_EXPORT int PLUGIN_CALL AmxUnload(AMX* amx)
 {
 	sampnode::amx::unload(amx);
-
 	return 1;
 }
